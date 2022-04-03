a957d86bd49dbbd762fe38f1da18c3dfa4b358aa
PotatingSloth <mintingtu@gmail.com>
2022-04-03 14:23:51 -0400

    UI + animations
    Added animations to the UI
    Added more road
    Linked the text on handle to the slider position

---
+++ SnomJam/Assets/Scripts/Menu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Text SpeedometerText;
+    public Slider SpeedometerSlider;
+    public GameObject MenuTitle;
+    public GameObject LerpStart;
+    public GameObject LerpEnd;
+
+    bool StartButtonClicked;
+    float timer;
+
+    private void Update()
+    {
+        SpeedometerText.text = SpeedometerSlider.value.ToString();
+        
+        if(StartButtonClicked)
+        {
+            timer = (timer + Time.deltaTime);
+            MenuTitle.transform.position = Vector3.Lerp(LerpStart.transform.position, LerpEnd.transform.position, timer);
+            if (timer >= 1)
+            {
+                StartButtonClicked = false;
+            }
+        }
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void OnStartButton()
+    {
+        StartButtonClicked = true;
+    }
+}
